96185130a6a58c81f049655a60fd8bbfacdc62e4
JOSalasT <jaime.os.salas@gmail.com>
2024-08-23 09:57:56 +0100

    Made changes so now you can choose different systems. Began work on form submission.

--- integration/views.py
+++ integration/views.py
@@ -5,4 +5,18 @@ from django.views.generic import ListView
 def index(request):
     return render(request, 'steps/upload_ontology.html')
 
+def process(request):
+    if request.method == 'POST':
+        ontology = request.POST.get('ontology')
+        for i in range(len(request.POST.get("data_source"))):
+            print("something")
+        for i in range(len(request.POST.get("local_source"))):
+            print("reading")
+        if request.POST.get('next') == "Materialise":
+            print("materialise")
+        elif request.POST.get('next') == "Run query":
+            print("run query")
+        else:
+            print("unknown next value")
+    return render(request, 'steps/upload_ontology.html')
 # Create your views here.
