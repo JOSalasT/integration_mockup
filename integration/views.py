6938494e3f3e6d23d08b0d22381d004ace32fc5e
JOSalasT <jaime.os.salas@gmail.com>
2024-08-23 10:02:32 +0100

    Began work on RML graph representation.

--- integration/views.py
+++ integration/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from pyrml import RMLConverter
+from rdflib import Graph
 from django.views.generic import ListView
 
 
 def index(request):
     return render(request, 'steps/upload_ontology.html')
+
+def rml_mapping(request):
+    if request.method == 'POST':
+        rml_file = request.FILES['rml_file']
+        rdf_graph = Graph()
+        rdf_graph.parse(rml_file, format='ttl')
+        nodes = []
+        edges = []
+        for s, p, o in rdf_graph:
+            node1 = s
+            nodes.append()
+            edge = p
+            node2 = o
+
+    return render(request,'steps/display_mapping.html')
 
 # Create your views here.
