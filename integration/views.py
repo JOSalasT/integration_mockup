40333936dfd714b8e0069a30b6cb1d00bbbb53dd
JOSalasT <jaime.os.salas@gmail.com>
2024-08-29 10:30:26 +0100

    Added an RML template in addition to the RML example there was before. Created a temporary directory inside project to store temporary files. RML mapping is working.

--- integration/views.py
+++ integration/views.py
@@ -1,14 +1,15 @@
 import os
-import subprocess
+import time
 
-from subprocess import Popen, PIPE
+from subprocess import run, Popen, PIPE
+
+import rdflib
 from django.shortcuts import render
-from pyrml import RMLConverter
 from rdflib import Graph
 from tempfile import NamedTemporaryFile
 from urllib.request import pathname2url
 
-from integration_mockup.settings import BASE_DIR
+from integration_mockup.settings import BASE_DIR, TEMP_DIR
 
 
 def index(request):
@@ -24,7 +25,6 @@ def rml_mapping(request):
         edges = []
         for s, p, o in rdf_graph:
             node1 = s
-            nodes.append()
             edge = p
             node2 = o
 
@@ -32,43 +32,48 @@ def rml_mapping(request):
 
 
 def jarWrapper(*args):
-    print(args)
-    javaProcess = Popen(['java', '-jar'] + list(args), stdout=PIPE, stderr=PIPE)
-    ret = []
-    while javaProcess.poll() is None:
-        line = javaProcess.stdout.readline()
-        line = line.decode('utf-8')
-        if line != '' and line.endswith("\n"):
-            ret.append(line[:-1])
-    stdout, stderr = javaProcess.communicate()
-    ret += stdout.decode('utf-8').split('\n')
-    if stderr != '':
-        ret += stderr.decode('utf-8').split('\n')
-    return ret
+    print(*args)
+    javaProcess = run(['java', '-jar'] + list(args), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    # while javaProcess.poll() is None:
+    #     line = javaProcess.stdout.readline()
+    #     line = line.decode('utf-8')
+    #     if line != '' and line.endswith("\n"):
+    #         ret.append(line[:-1])
+    # stdout, stderr = javaProcess.communicate()
+    # ret += stdout.decode('utf-8').split('\n')
+    # if stderr != '':
+    #     ret += stderr.decode('utf-8').split('\n')
+    return javaProcess.stdout.decode('utf-8')
 
 
 def process(request):
     if request.method == 'POST':
-        ontology = request.POST.get('ontology')
+        ontology = request.POST.get('ontology').encode('utf-8')
+        ontology_graph = rdflib.Graph()
+        ontology_graph.parse(ontology, format='xml')
+        for s, p, o in ontology_graph:
+            print(str(s) + "," + str(p) + "," + str(o))
         with request.FILES.get('rml_file') as rml_file:
-            temp_rml_file = NamedTemporaryFile(mode='w+', encoding='utf-8', delete=False)
+            extension = "." + rml_file.name.rsplit('.', 1)[1]
+            temp_rml_file = NamedTemporaryFile(mode='w+', encoding='utf-8', newline="\n", delete=False, dir=TEMP_DIR, suffix=extension)
             template_vars = dict()
             temp_files = []
             for file in request.FILES.getlist('local_source'):
-                temp_file = NamedTemporaryFile(mode='wb', delete=False)
-                template_vars[file.name[:file.name.rfind(".")]] = pathname2url(temp_file.name)
+                temp_extension = "." + file.name.rsplit('.', 1)[1]
+                temp_file = NamedTemporaryFile(mode='w+', encoding='utf-8', newline="\n", delete=False, dir=TEMP_DIR, suffix=temp_extension)
+                template_vars[file.name[:file.name.rfind(".")]] = temp_file.name
                 for chunk in file.chunks():
-                    temp_file.write(chunk)
+                    temp_file.write(chunk.decode('utf-8'))
                 temp_files.append(temp_file)
+                temp_file.seek(0)
             for chunk in rml_file.chunks():
                 temp_string = chunk.decode('utf-8').replace('\r', '')
                 for key, value in template_vars.items():
                     temp_string = temp_string.replace("{{ " + key + " }}", value)
                 temp_rml_file.write(temp_string)
             temp_rml_file.seek(0)
-        java_args = [os.path.join(BASE_DIR, "integration_mockup", "static", "jar", "rmlmapper-7.0.0-r374-all.jar")]
-        rml_result = jarWrapper(*java_args)
-        print(rml_result)
+            java_args = [os.path.join(BASE_DIR, "integration_mockup", "static", "jar", "rmlmapper-7.0.0-r374-all.jar"), "-m", temp_rml_file.name]
+            rml_result = jarWrapper(*java_args)
         if "data_source" in request.POST:
             for i in range(len(request.POST.get("data_source"))):
                 print("something")
@@ -95,8 +100,7 @@ def process(request):
                     print("Running GQR")
                 elif system == "OntopRW":
                     print("Running OntopRW")
-                    process = Popen(['java', '-jar', 'tika-app-1.24.1.jar', '-t', '42250_EN_Upload.docx'], stdout=PIPE,
-                                    stderr=PIPE)
+                    process = run(['java', '-jar', 'tika-app-1.24.1.jar', '-t', '42250_EN_Upload.docx'])
                     result = process.communicate()
                 elif system == "CGQR":
                     print("Running CGQR")
--- integration_mockup/settings.py
+++ integration_mockup/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "integration_mockup/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "integration_mockup", "static")]
+TEMP_DIR = os.path.join(BASE_DIR, "integration_mockup", "temp")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
